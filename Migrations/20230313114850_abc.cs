d6722397eefb9fef8b5987ceb3c734f1257b1fec
jervson <henri.jervson@gmail.com>
2023-03-13 13:52:34 +0200

    add more packages and organize files

---
+++ Migrations/20230313114850_abc.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HenriJervsonGrainWarehouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class abc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cargo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnteringMass = table.Column<double>(type: "float", nullable: false),
+                    LeavingMass = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargo", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cargo");
+        }
+    }
+}
