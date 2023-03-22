3a839cc9d55424a2038705d3f1a049a0115b74e1
Jervson <henri.jervson@gmail.com>
2023-03-23 00:51:09 +0200

    add TotalMass to WarehouseViewModel, make it equal EnterMass-Leavingmass and add it to the View

--- Models/WarehouseViewModel.cs
+++ Models/WarehouseViewModel.cs
@@ -8,6 +8,5 @@ namespace HenriJervsonGrainWarehouse.Models
         public List<Cargo> Cargos { get; set; }
         public SelectList CarNumbers { get; set; }
         public string CarNumber { get; set; }
-        public double TotalMass { get; set; }
     }
 }
