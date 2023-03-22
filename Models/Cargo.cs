c6e78a357e878206e56e477d283abe3f4b7ad5e1
Jervson <henri.jervson@gmail.com>
2023-03-23 00:40:44 +0200

    add data annotation

--- Models/Cargo.cs
+++ Models/Cargo.cs
@@ -1,10 +1,14 @@
-﻿namespace HenriJervsonGrainWarehouse.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HenriJervsonGrainWarehouse.Models
 {
     public class Cargo
     {
         public int Id { get; set; }
         public string CarNumber { get; set; }
+        [Range(4, 500)]
         public double EnteringMass { get; set; }
+        [Range(4, 500)]
         public double? LeavingMass { get; set; }
     }
 }
